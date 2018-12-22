f5507991dca11ddb3c80cab4412d3dccb1fccfc0
RavinduSachintha <ravindusachintha53@gmail.com>
2018-12-23 00:14:37 +0530

    update css files

--- src/core/CoreController.java
+++ src/core/CoreController.java
@@ -1,30 +1,71 @@
 package core;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.Pane;
+import javafx.stage.DirectoryChooser;
+
+import java.io.File;
 
 public class CoreController {
 
     /* Declaration of panes for navigation */
-    public Pane panLocation;
-    public Pane panTemplate;
-    public Pane panProject;
-    public Pane panDevelopers;
-    public Pane panContributions;
-    public Pane panLicense;
-    public Pane panMiscellaneous;
-    public Pane panFinish;
+    @FXML
+    private Pane panLocation;
+    @FXML
+    private Pane panTemplate;
+    @FXML
+    private Pane panProject;
+    @FXML
+    private Pane panDevelopers;
+    @FXML
+    private Pane panContributions;
+    @FXML
+    private Pane panLicense;
+    @FXML
+    private Pane panMiscellaneous;
+    @FXML
+    private Pane panFinish;
 
     /* Declaration of buttons for navigational panes */
-    public Button btnNavLocation;
-    public Button btnNavTemplate;
-    public Button btnNavProject;
-    public Button btnNavDevelopers;
-    public Button btnNavContributions;
-    public Button btnNavLicense;
-    public Button btnNavMiscellaneous;
-    public Button btnNavFinish;
+    @FXML
+    private Button btnNavLocation;
+    @FXML
+    private Button btnNavTemplate;
+    @FXML
+    private Button btnNavProject;
+    @FXML
+    private Button btnNavDevelopers;
+    @FXML
+    private Button btnNavContributions;
+    @FXML
+    private Button btnNavLicense;
+    @FXML
+    private Button btnNavMiscellaneous;
+    @FXML
+    private Button btnNavFinish;
+
+    /* Declarations related to location pane */
+    @FXML
+    private ComboBox cmbFileLocation;
+    @FXML
+    private Button btnSetNewLocation;
+    @FXML
+    private ComboBox cmbFileName;
+    @FXML
+    private Button btnSaveLocationPane;
+
+
+    /*
+     * Functions' definitions related to Main Window and all Panes
+     */
+
+    /* FXML file initializing function */
+    public void initialize() {
+        initializeFileLocations();
+    }
 
 
     /* Function for handle button actions on navigation */
@@ -49,4 +90,34 @@ public class CoreController {
             System.err.println("Error on navigation button action event");
         }
     }
+
+
+
+    /*
+     * Functions' definitions related to Location Pane
+     */
+
+    /* Initialize file location choice box values */
+    private void initializeFileLocations() {
+        // Add the recent location list
+        cmbFileLocation.getItems().addAll("Location 01", "Location 02", "Location 03");
+
+        // Set the default location
+        cmbFileLocation.getSelectionModel().selectFirst();
+    }
+
+    /* Function for handle button actions on set new location button */
+    public void handleSetNewLocationButtonAction(ActionEvent actionEvent) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        File selectedDirectory = directoryChooser.showDialog(panLocation.getScene().getWindow());
+
+        if (selectedDirectory == null) {
+            //No Directory selected
+        } else {
+            System.out.println(selectedDirectory.getAbsolutePath());
+        }
+
+
+    }
+
 }
